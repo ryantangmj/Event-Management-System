24a28ae9da1b66427c50e87cd048acb9806221aa
ryantangmj <110431837+ryantangmj@users.noreply.github.com>
2024-02-16 15:44:31 +0800

    Added password validation on createaccount page

--- EventManagementSystem/EventManagementSystem-war/src/java/managedbean/AccountManagedBean.java
+++ EventManagementSystem/EventManagementSystem-war/src/java/managedbean/AccountManagedBean.java
@@ -9,6 +9,8 @@ import javax.inject.Named;
 import javax.faces.view.ViewScoped;
 import java.io.Serializable;
 import javax.ejb.EJB;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
 import javax.faces.event.ActionEvent;
 import session.AccountSessionLocal;
 
@@ -19,12 +21,32 @@ import session.AccountSessionLocal;
 @Named(value = "accountManagedBean")
 @ViewScoped
 public class AccountManagedBean implements Serializable {
+
     @EJB
     private AccountSessionLocal accountSession;
     private String name;
     private String contactDetails;
     private String email;
     private String password;
+    private String validatePassword;
+
+    /**
+     * Get the value of validatePassword
+     *
+     * @return the value of validatePassword
+     */
+    public String getValidatePassword() {
+        return validatePassword;
+    }
+
+    /**
+     * Set the value of validatePassword
+     *
+     * @param validatePassword new value of validatePassword
+     */
+    public void setValidatePassword(String validatePassword) {
+        this.validatePassword = validatePassword;
+    }
 
     public String getName() {
         return name;
@@ -57,20 +79,32 @@ public class AccountManagedBean implements Serializable {
     public void setPassword(String password) {
         this.password = password;
     }
+
     /**
      * Creates a new instance of AccountManagedBean
      */
     public AccountManagedBean() {
     }
-    
-    public void createAccount(ActionEvent evt) {
+
+    public void createAccount() {
         Account account = new Account();
         account.setName(name);
         account.setContactDetails(contactDetails);
         account.setEmail(email);
         account.setPassword(password);
-        
+
         accountSession.createAccount(account);
     }
-    
+
+    public String validateAndCreateAccount() {
+        if (password == null || !password.equals(validatePassword)) {
+            FacesContext.getCurrentInstance().addMessage(null,
+                    new FacesMessage(FacesMessage.SEVERITY_ERROR, "Password mismatch", "The passwords do not match."));
+            return null; // Stay on the page due to validation failure
+        } else {
+            createAccount();
+            return "login.xhtml?faces-redirect=true"; // Navigate on success
+        }
+    }
+
 }
